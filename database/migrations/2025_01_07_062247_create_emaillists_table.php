d0b63c036baa999684b599239ee8943149be2d1c
muntedher2022 <muntedher099@gmail.com>
2025-10-11 13:17:25 +0300

    mun اخر تعديل

--- database/migrations/2025_01_07_062247_create_emaillists_table.php
+++ database/migrations/2025_01_07_062247_create_emaillists_table.php
@@ -6,29 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-
         Schema::create('emaillists', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->nullable()->comment('رقم المستخدم');
+            $table->enum('type', ['section', 'department'])->default('section')->comment('نوع القائمة: قسم أو فرع');
             $table->string('department')->nullable()->comment('القسم');
             $table->string('email')->nullable()->comment('البريد الإلكتروني');
             $table->text('notes')->nullable()->comment('ملاحظات');
             $table->timestamps();
+
+
             $table->index('department');
             $table->index('email');
         });
-
-
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('emaillists');
